2b6848931955956563949cff18e12e6cac3eee75
Aris Setiawan <arissetia.m@gmail.com>
2025-02-01 23:29:17 +0700

    feat: seo focus on frontpage

---
+++ components/ui/tech-logos.tsx
@@ -0,0 +1,149 @@
+"use client"
+
+import Image from 'next/image'
+
+export const techLogos = [
+  { 
+    name: "Eton House", 
+    id: 1, 
+    img: () => (
+      <Image
+        src="/images/clients/eton-house.png"
+        alt="Eton House"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Rahardi Creative", 
+    id: 2, 
+    img: () => (
+      <Image
+        src="/images/clients/rahardi-creative.png"
+        alt="Rahardi Creative"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Picastock", 
+    id: 3, 
+    img: () => (
+      <Image
+        src="/images/clients/picastock.png"
+        alt="Picastock"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Canvas Garment", 
+    id: 4, 
+    img: () => (
+      <Image
+        src="/images/clients/canvas-garment.png"
+        alt="Canvas Garment"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Bacakomik", 
+    id: 5, 
+    img: () => (
+      <Image
+        src="/images/clients/bacakomik.png"
+        alt="Bacakomik"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Mary Jardin", 
+    id: 6, 
+    img: () => (
+      <Image
+        src="/images/clients/maryjardin.png"
+        alt="Mary Jardin"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Ortensia Village", 
+    id: 7, 
+    img: () => (
+      <Image
+        src="/images/clients/ortensia-village.png"
+        alt="Ortensia Village"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Learn Islam", 
+    id: 8, 
+    img: () => (
+      <Image
+        src="/images/clients/learnislam.png"
+        alt="Learn Islam"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "D'Holistic Care", 
+    id: 9, 
+    img: () => (
+      <Image
+        src="/images/clients/dholistic-care.png"
+        alt="D'Holistic Care"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Saudagar Properti", 
+    id: 10, 
+    img: () => (
+      <Image
+        src="/images/clients/saudagar-properti.png"
+        alt="Saudagar Properti"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  },
+  { 
+    name: "Cipika", 
+    id: 11, 
+    img: () => (
+      <Image
+        src="/images/clients/cipika.png"
+        alt="Cipika"
+        width={180}
+        height={60}
+        className="object-contain"
+      />
+    )
+  }
+]; 
